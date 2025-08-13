dd986ecd4a865a06ec1ffb3cd7eed9e005349b95
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-08-13 22:02:04 +0500

    background color change on button click

---
+++ ChangeBackground/app.js
@@ -0,0 +1,14 @@
+
+const btn = document.getElementById("btn")
+const main = document.getElementById("main")
+
+btn.addEventListener('click',()=>{
+
+  
+    let letters = "0123456789ABCDEF";
+            let color = "#";
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+document.body.style.backgroundColor =color
+})
